eb3bfba078ac2cceeb17dd28e5b43a36bf50b784
K16E, Inc <kb@k16e.co>
2024-10-21 23:58:59 +0100

    ...

---
+++ src/scripts/empty.js
@@ -0,0 +1,8 @@
+export default function _empty(value) {
+    return (
+        value === undefined ||
+        value === null ||
+        (typeof value === "object" && Object.keys(value).length === 0) ||
+        (typeof value === "string" && value.trim().length === 0)
+    )
+}
