9308796b7892eeee63a843d980830ccd10ec1944
K16E, Inc <kb@k16e.co>
2024-10-22 15:10:07 +0100

    ...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,14 @@ export default {
                 sans: ['Instrument Sans Variable, sans-serif', ...defaultTheme.fontFamily.sans],
                 display: 'Urbanist Variable, sans-serif'
             }
+        },
+        screens: {
+            'sm': '576px',
+            'md': '960px',
+            'lg': '1280px',
+            'xl': '1440px',
+            '2xl': '1600px',
+            '3xl': '1920px',
         }
     },
     plugins: [
