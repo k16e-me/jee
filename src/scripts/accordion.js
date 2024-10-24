ce688df626921b631f9cce4b49b6d1a3a0d6c614
K16E, Inc <kb@k16e.co>
2024-10-24 07:10:51 +0100

    accordion done

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -13,27 +13,42 @@ export default function _active() {
             icon = _q('.accordion__icon', menu),
             content = _q('.accordion__content', menu)
 
-        if (trigger && icon && content) {
-            trigger.addEventListener('click', e => {
-                if (!menu.classList.contains('active')) {
-                    menu.classList.add('active')
-                    trigger.setAttribute('aria-expanded', 'true')
-                    content.classList.remove('hidden')
-                    content.style.maxHeight =
-                        content.scrollHeight + 'px'
-                    icon.classList.add('rotate-180')
-                } else {
-                    menu.classList.remove('active')
-                    trigger.setAttribute('aria-expanded', 'false')
-                    content.style.maxHeight = '0px'
-                    icon.classList.remove('rotate-180')
-                    setTimeout(() => {
-                        content.classList.add('hidden')
-                    }, 250)
-                }
-                e.preventDefault()
-                return false
-            })
-        }
+        trigger.addEventListener('click', e => {
+            e.preventDefault()
+            let actives = _siblings(menu).filter(el => el.classList.contains('active'))
+            if (!menu.classList.contains('active')) {
+                expand(menu, trigger, content, icon)
+                actives.forEach(el => {
+                    const
+                        trigger = _q('.accordion__button', el),
+                        icon = _q('.accordion__icon', el),
+                        content = _q('.accordion__content', el)
+                    collapse(el, trigger, content, icon)
+                })
+            } else {
+                collapse(menu, trigger, content, icon)
+            }
+
+            return false
+        })
     })
+
+    function expand(menu, trigger, content, icon) {
+        menu.classList.add('active')
+        trigger.setAttribute('aria-expanded', 'true')
+        content.classList.remove('hidden')
+        content.style.maxHeight =
+            content.scrollHeight + 'px'
+        icon.classList.add('rotate-180')
+    }
+
+    function collapse(menu, trigger, content, icon) {
+        menu.classList.remove('active')
+        trigger.setAttribute('aria-expanded', 'false')
+        content.style.maxHeight = '0px'
+        icon.classList.remove('rotate-180')
+        setTimeout(() => {
+            content.classList.add('hidden')
+        }, 250)
+    }
 }
