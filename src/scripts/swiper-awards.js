06edf1de811ca4aa8c697f2c3646772397791b90
K16E, Inc <kb@k16e.co>
2024-10-25 13:02:02 +0100

    swiper looking juicy

--- src/scripts/swiper-awards.js
+++ src/scripts/swiper-awards.js
@@ -8,13 +8,33 @@ export default function _swiperAwards() {
     if (!_q('.swiper-awards')) return
 
     const swiper = new Swiper('.swiper-awards', {
-        slidesPerView: 7,
-        spaceBetween: 32,
         mousewheel: true,
         loop: true,
         autoplay: {
             delay: 5000,
             pauseOnMouseEnter: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 5,
+                spaceBetween: 16
+            },
+            576: {
+                slidesPerView: 5,
+                spaceBetween: 24
+            },
+            768: {
+                slidesPerView: 6,
+                spaceBetween: 32
+            },
+            960: {
+                slidesPerView: 5,
+                spaceBetween: 32
+            },
+            1200: {
+                slidesPerView: 7,
+                spaceBetween: 32
+            }
         }
     })
 }
