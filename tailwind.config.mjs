5680ea0f52d1b0c7de033ce71ad11eca3bb0403f
K16E, Inc <kb@k16e.co>
2024-10-26 21:44:21 +0100

    tested image hanging text grid, all kosher

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -22,7 +22,7 @@ export default {
         },
         screens: {
             'sm': '640px',
-            'md': '960px',
+            'md': '800px',
             'lg': '1200px',
             'xl': '1440px',
             '2xl': '1600px',
