65ba74ad84915e2b1f8c8fe75b964bcc22f00ba5
K16E, Inc <kb@k16e.co>
2024-10-26 23:31:43 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,6 +40,7 @@ export default defineConfig({
 
                 // Sections
                 imageHangingText: 'composites/sections/ImageHangingText',
+                sectionBentobox: 'composites/sections/SectionBentobox',
             }
         })
     ],
