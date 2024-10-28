eaa31107b29e0680a369dbd3f012b95ee41880da
K16E, Inc <kb@k16e.co>
2024-10-28 08:56:13 +0100

    ...

--- src/scripts/swiper-quotes.js
+++ src/scripts/swiper-quotes.js
@@ -2,23 +2,29 @@ import {
     _q,
 } from './snips'
 import Swiper from 'swiper'
-import { Navigation, Autoplay } from 'swiper/modules'
+import { Navigation, Autoplay, Pagination } from 'swiper/modules'
 import 'swiper/css'
+import 'swiper/css/pagination'
 
 export default function _swiperQuotes() {
     if (!_q('.swiper-quotes')) return
 
     const swiper = new Swiper('.swiper-quotes', {
-        modules: [Navigation, Autoplay],
+        modules: [Navigation, Autoplay, Pagination],
         navigation: {
             nextEl: '.swiper-next'
         },
         loop: true,
         grabCursor: true,
         autoplay: {
-            delay: 5000,
+            delay: 15000,
             disableOnInteraction: false,
             pauseOnMouseEnter: true
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
         }
     })
 }
