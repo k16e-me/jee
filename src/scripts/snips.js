a78114a90b63d013c58051afd1e3549b37a92224
K16E, Inc <kb@k16e.co>
2024-10-29 18:41:22 +0100

    ...

--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -10,8 +10,9 @@ const
         if (direction === 'bottom') return new Array('pb-8', 'md:pb-14', 'lg:pb-20')
         if (direction === 'both') return new Array('py-8', 'md:py-14', 'lg:py-20')
     },
-    _xPadding = (edges = 'both') => {
+    _xPadding = (edges = 'both', media = null) => {
         if (edges === 'both') return new Array('px-3', 'md:px-8', 'lg:px-16')
+        if (media === 'md-up') return new Array('md:px-8', 'lg:px-16')
     },
     _gridGap = (direction = 'y') => {
         if (direction === 'y') return new Array('gap-y-8', 'md:gap-y-14', 'lg:gap-y-20')
