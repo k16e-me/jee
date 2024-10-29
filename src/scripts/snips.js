930919c7ee0f2d4df11b8df72b74bfc8db173f9f
K16E, Inc <kb@k16e.co>
2024-10-29 18:08:22 +0100

    sticky position achieved with wrapper catchall

--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -6,15 +6,15 @@ const
     _odd = n => Math.abs(n % 2) == 1,
     _capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase(),
     _sectionPadding = (direction = 'top') => {
-        if (direction === 'top') return new Array('pt-10', 'md:pt-14', 'lg:pt-20')
-        if (direction === 'bottom') return new Array('pb-10', 'md:pb-14', 'lg:pb-20')
-        if (direction === 'both') return new Array('py-10', 'md:py-14', 'lg:py-20')
+        if (direction === 'top') return new Array('pt-8', 'md:pt-14', 'lg:pt-20')
+        if (direction === 'bottom') return new Array('pb-8', 'md:pb-14', 'lg:pb-20')
+        if (direction === 'both') return new Array('py-8', 'md:py-14', 'lg:py-20')
     },
     _xPadding = (edges = 'both') => {
         if (edges === 'both') return new Array('px-3', 'md:px-8', 'lg:px-16')
     },
     _gridGap = (direction = 'y') => {
-        if (direction === 'y') return new Array('gap-y-10', 'md:gap-y-14', 'lg:gap-y-20')
+        if (direction === 'y') return new Array('gap-y-8', 'md:gap-y-14', 'lg:gap-y-20')
     }
 
 export {
