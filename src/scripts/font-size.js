1fd86fcea1ca38d18d175209ed3c3326f611dc48
K16E, Inc <kb@k16e.co>
2024-10-30 19:39:32 +0100

    stack highlights done

---
+++ src/scripts/font-size.js
@@ -0,0 +1,17 @@
+const
+    body = 'text-[1.05rem] leading-[1.4975rem]',
+    copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.125rem] lg:leading-[1.65rem]',
+    emphasis = 'text-[1.0625rem] lg:text-[1.0325rem] leading-[1.4925rem]',
+    blurb = 'text-[0.9375rem] leading-[1.125rem]',
+
+    _fontSize = (s = 'body') => {
+        if (s === 'body') return [...body.split(' ')]
+        if (s === 'copy') return [...copy.split(' ')]
+        if (s === 'emphasis') return [...emphasis.split(' ')]
+        if (s === 'blurb') return [...blurb.split(' ')]
+        return false
+    }
+
+export {
+   _fontSize
+}
--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -1,4 +1,6 @@
 const
+    topPadding = 'pt-8 md:pt-14 lg:pt-20',
+
     _body = () => document.body,
     _q = (selector, parent) => (parent ? parent : document).querySelector(selector),
     _ql = (selector, parent) => Array.from((parent ? parent : document).querySelectorAll(selector)),
@@ -6,7 +8,7 @@ const
     _odd = n => Math.abs(n % 2) == 1,
     _capitalize = str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase(),
     _sectionPadding = (direction = 'top') => {
-        if (direction === 'top') return ['pt-8', 'md:pt-14', 'lg:pt-20']
+        if (direction === 'top') return [...topPadding.split(' ')]
         if (direction === 'bottom') return ['pb-8', 'md:pb-14', 'lg:pb-20']
         if (direction === 'both') return ['py-8', 'md:py-14', 'lg:py-20']
     },
@@ -14,6 +16,7 @@ const
         if (edges === 'both') return ['px-3', 'md:px-8', 'lg:px-16']
         if (edges === 'right') return ['pr-3', 'md:pr-8', 'lg:pr-16']
         if (edges === 'both-mdup') return ['md:px-8', 'lg:px-16']
+        if (edges === 'both-lgdown') return ['px-3', 'md:px-8', 'lg:px-16', 'xl:pr-0']
     },
     _gridGap = (direction = 'y') => {
         if (direction === 'y') return ['gap-y-8', 'md:gap-y-14', 'lg:gap-y-20']
