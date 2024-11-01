3efc0ce49bfd5e523ba24ffbd82eff8286a5f0b4
K16E, Inc <kb@k16e.co>
2024-11-01 10:33:34 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -1,13 +1,13 @@
 const
     s4 = 'text-[1.55625rem] leading-[1.75rem] md:text-[1.795rem] md:leading-[1.875rem] lg:text-[1.9rem] lg:leading-[2.05rem]',
-    s6 = 'text-[1.1875rem] leading-[1.3125rem]',
+    s6 = 'text-[1.1875rem] leading-[1.3725rem]',
     s4Upper = 'text-[1.8875rem] leading-[2rem] md:text-[1.9175rem]',
     sx1 = 'text-[1.1995rem] leading-[1.495rem] sm:text-[1.275rem] lg:text-[1.3225rem] lg:leading-[1.475rem]',
-    body = 'text-[1.05rem] leading-[1.4975rem]',
+    body = 'text-[1.025rem] leading-[1.4975rem]',
     copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.125rem] lg:leading-[1.65rem]',
     emphasis = 'text-[1.0625rem] lg:text-[1.0325rem] leading-[1.4925rem]',
     blurb = 'text-[0.9375rem] leading-[1.125rem]',
-    footnote = 'text-[0.9175rem] leading-[1.0625rem]',
+    footnote = 'text-[0.8975rem] leading-[1.0625rem]',
 
     _fontSize = (s = 'body') => {
         if (s === 's4') return [...s4.split(' ')]
