fcccee786373e57f15b07252a9d1a9685a6a882b
K16E, Inc <kb@k16e.co>
2024-11-04 21:47:13 +0100

    ...

--- src/scripts/scrollspy.js
+++ src/scripts/scrollspy.js
@@ -2,7 +2,29 @@ import { _ql, _q } from '@scripts/snips'
 
 
 export default function _scrollspy() {
-    if (!_q('[data-spy-sections]')) return
+    if (!_q('[data-spy]')) return
 
-    
+    const
+        sectionVisibility = {},
+        observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                sectionVisibility[entry.target.id] = entry.intersectionRatio
+            })
+
+            const
+                maxVisibility = Math.max(...Object.values(sectionVisibility)),
+                [activeId, intersectionRatio] = Object
+                    .entries(sectionVisibility)
+                    .find(([id, visibility]) => visibility == maxVisibility)
+
+            _ql('[data-spy-sections] a').forEach(a => {
+                a.classList.remove('is-in-view')
+            })
+            _q(`[data-spy-sections] a[href='#${activeId}']`).classList.add('is-in-view')
+        }, { threshold: [0.33, 0.66, 1] }),
+        sections = _ql('[data-spy] section')
+
+    sections.forEach(section => {
+        observer.observe(section)
+    })
 }
