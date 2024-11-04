60a513e3e4f7d95071352fd753f9419c6a1e9f20
K16E, Inc <kb@k16e.co>
2024-11-04 23:09:26 +0100

    started people

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -2,6 +2,8 @@ import { atom } from 'nanostores'
 import { persistentAtom } from '@nanostores/persistent'
 
 export const
-    _awards = atom([])
-    
+    _awards = atom([]),
+    _people= atom([])
+
 export function _addAwards(item) { _awards.set(item) }
+export function _addPeople(item) { _people.set(item) }
