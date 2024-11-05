c730d31c68cf66b2fb710fd8726d5b443a7c2bc3
K16E, Inc <kb@k16e.co>
2024-11-05 21:00:36 +0100

    sectors added

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -5,10 +5,14 @@ export const
     _awards = atom([]),
     _people = atom([]),
     _designations = atom([]),
-    _offices = atom([])
+    _offices = atom([]),
+    _sectors = atom([]),
+    _practices = atom([])
 
 
 export function _addAwards(item) { _awards.set(item) }
 export function _addPeople(item) { _people.set(item) }
 export function _addDesignations(item) { _designations.set(item) }
 export function _addOffices(item) { _offices.set(item) }
+export function _addSectors(item) { _sectors.set(item) }
+export function _addPractices(item) { _practices.set(item) }
