00ba26b33355531f1adf095a636876d128932a9d
K16E, Inc <kb@k16e.co>
2024-11-05 14:58:09 +0100

    sorting by rank looks okay

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -3,7 +3,10 @@ import { persistentAtom } from '@nanostores/persistent'
 
 export const
     _awards = atom([]),
-    _people= atom([])
+    _people= atom([]),
+    _designtions= atom([])
+
 
 export function _addAwards(item) { _awards.set(item) }
 export function _addPeople(item) { _people.set(item) }
+export function _addDesignations(item) { _designtions.set(item) }
