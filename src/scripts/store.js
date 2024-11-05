e6a73b4df9ba717449f83128a2588f24a8747915
K16E, Inc <kb@k16e.co>
2024-11-05 20:29:22 +0100

    locations added

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -3,10 +3,12 @@ import { persistentAtom } from '@nanostores/persistent'
 
 export const
     _awards = atom([]),
-    _people= atom([]),
-    _designtions= atom([])
+    _people = atom([]),
+    _designations = atom([]),
+    _offices = atom([])
 
 
 export function _addAwards(item) { _awards.set(item) }
 export function _addPeople(item) { _people.set(item) }
-export function _addDesignations(item) { _designtions.set(item) }
+export function _addDesignations(item) { _designations.set(item) }
+export function _addOffices(item) { _offices.set(item) }
