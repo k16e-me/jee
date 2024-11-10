15271beb103420e05cb8e7b3f79e4ab30e5173e1
K16E, Inc <kb@k16e.co>
2024-11-10 22:27:40 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -1,8 +1,10 @@
 const
     s4 = 'text-[1.55625rem] leading-[1.75rem] md:text-[1.795rem] md:leading-[1.875rem] lg:text-[1.9rem] lg:leading-[2.05rem]',
+    s5 = 'text-[1.3125rem] leading-[1.5rem] md:text-[1.5625rem] md:leading-[1.75rem] lg:text-[1.6875rem] lg:leading-[1.9375rem]',
     s6 = 'text-[1.1875rem] leading-[1.3725rem]',
     s4Upper = 'text-[1.8875rem] leading-[2rem] md:text-[1.9175rem]',
     sx1 = 'text-[1.1995rem] leading-[1.495rem] md:text-[1.2rem] md:leading-[1.475rem]',
+    sx2 = 'text-[1.2995rem] leading-[1.495rem] md:text-[1.4rem] md:leading-[1.5rem]',
     body = 'text-[1.025rem] leading-[1.4975rem]',
     copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.125rem] lg:leading-[1.65rem]',
     emphasis = 'text-[1.0625rem] lg:text-[1.0325rem] leading-[1.4925rem]',
@@ -11,9 +13,11 @@ const
 
     _fontSize = (s = 'body') => {
         if (s === 's4') return [...s4.split(' ')]
+        if (s === 's5' || s === '5') return [...s5.split(' ')]
         if (s === 's6' || s === '6') return [...s6.split(' ')]
         if (s === 's4-upper') return [...s4Upper.split(' ')]
         if (s === 'sx1' || s === '1hx') return [...sx1.split(' ')]
+        if (s === 'sx2' || s === '2hx') return [...sx2.split(' ')]
         if (s === 'body') return [...body.split(' ')]
         if (s === 'copy') return [...copy.split(' ')]
         if (s === 'emphasis') return [...emphasis.split(' ')]
