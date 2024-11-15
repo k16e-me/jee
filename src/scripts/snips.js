af2cbe93c5914cbff3253d1f9c1dc843e1a05f36
K16E, Inc <kb@k16e.co>
2024-11-15 05:55:01 +0100

    adjusted padding on related people on person

--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -24,6 +24,7 @@ const
         if (edges === 'both') return ['px-3', 'md:px-8', 'lg:px-16']
         if (edges === 'right') return ['pr-3', 'md:pr-8', 'lg:pr-16']
         if (edges === 'left') return ['pl-3', 'md:pl-8', 'lg:pl-16']
+        if (edges === 'left-md-down') return ['pl-3', 'md:pl-8', 'lg:pl-0']
         if (edges === 'both-mdup') return ['md:px-8', 'lg:px-16']
         if (edges === 'both-mddown-lg-l') return ['px-3', 'md:px-8', 'lg:pl-0', 'lg:pr-16']
         if (edges === 'both-md-down-l') return ['px-3', 'md:px-8', 'lg:!pr-0', 'lg:pl-16']
