4dfa18ec88ac767a6ac574f3b1b2300ac3ccfdaf
K16E, Inc <kb@k16e.co>
2024-11-16 07:12:17 +0100

    laid groundowrk successes carousel, image in

--- src/scripts/snips.js
+++ src/scripts/snips.js
@@ -7,7 +7,7 @@ const
     topPaddingXlUp = 'xl:pt-20',
     edgePaddingXlUpR = 'xl:pr-16',
     edgePaddingXlUpL = 'xl:pl-16',
-    headerAndBlockSpacing = 'space-y-3.5 md:space-y-4 lg:space-y-5',
+    headerAndBlockSpacing = 'space-y-3.5 md:space-y-4 lg:space-y-6',
     gapY = 'gap-y-7 xs:gap-y-8 md:gap-y-10 ml:gap-y-14 lg:gap-y-20',
 
     _body = () => document.body,
