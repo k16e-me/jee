6ffc7ec66867039efed6a3798b0978744f215957
K16E, Inc <kb@k16e.co>
2024-11-18 16:38:09 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -5,7 +5,7 @@ const
     s6 = 'text-[1.1975rem] leading-[1.3725rem]',
     s4Upper = 'text-[1.8875rem] leading-[2rem] md:text-[1.9175rem]',
     sx1 = 'text-[1.1775rem] leading-[1.495rem] md:text-[1.15rem]',
-    sx2 = 'text-[1.35rem] leading-[1.45rem] md:text-[1.3rem]',
+    sx2 = 'text-[1.25rem] leading-[1.45rem] md:text-[1.3rem] md:leading-[1.65rem]',
     copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.125rem] lg:leading-[1.55rem]',
     emphasis = 'text-[1.0625rem] lg:text-[1.0325rem] leading-[1.385rem]',
     body = 'text-[1.025rem] leading-[1.4975rem]',
