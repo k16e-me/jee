ac417190062b9cd9ed239bd7028d30ceb0b9a984
K16E, Inc <kb@k16e.co>
2024-11-25 05:13:02 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -5,7 +5,7 @@ const
     s5 = 'text-[1.3125rem] leading-[1.45rem] md:text-[1.4625rem] md:leading-[1.75rem] lg:text-[1.5275rem] lg:leading-[1.7375rem]',
     s6 = 'text-[1.0875rem] leading-[1.3725rem]',
     s4Upper = 'text-[1.715rem] leading-[1.9rem] md:text-[1.815rem]',
-    sx1 = 'text-[1.075rem] leading-[1.495rem] md:text-[1.0975rem]',
+    sx1 = 'text-[1.015rem] leading-[1.455rem] md:text-[1.0875rem]',
     sx2 = 'text-[1.25rem] leading-[1.45rem] md:text-[1.3rem] md:leading-[1.65rem]',
     copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.125rem] lg:leading-[1.55rem]',
     emphasis = 'text-[1.0525rem] leading-[1.475rem]',
