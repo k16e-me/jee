0abfff64bca6327337364d4b8132b969d90d9e0e
K16E, Inc <kb@k16e.co>
2024-12-07 08:59:01 +0100

    core functionality, behavior modal-video good

--- src/scripts/modal-video.js
+++ src/scripts/modal-video.js
@@ -6,23 +6,29 @@ export default function _modalVideo() {
 
     const trigger = _ql('[data-modal-video-play]')
     const modal = _q('[data-modal-video]')
-    const close = _q('[data-modal-close]', modal),
-        on = e => {
-            const
-                target = e.target
+    const close = _q('[data-modal-close]', modal)
+    const video = _q('iframe[src*="www.youtube.com"]', modal)
 
-            modal.classList.remove('translate-y-16', 'scale-50', 'opacity-0', 'invisible')
-            modal.classList.add('translate-y-0', 'opacity-100')
+    const on = e => {
+        const target = e.target
 
-            _setShim()
-        },
-        off = () => {
-            modal.classList.remove('translate-y-0', 'opacity-100')
-            modal.classList.add('translate-y-16', 'scale-50', 'opacity-0')
-            setTimeout(() => modal.classList.add('invisible'), 300)
+        modal.classList.remove('translate-y-16', 'scale-50', 'opacity-0', 'invisible')
+        modal.classList.add('translate-y-0', 'opacity-100')
 
-            _unsetShim()
-        }
+        video.src = video.src + (video.src.indexOf('?') < 0 ? '?' : '&') + 'autoplay=1'
+
+        _setShim()
+    }
+
+    const off = () => {
+        modal.classList.remove('translate-y-0', 'opacity-100')
+        modal.classList.add('translate-y-16', 'scale-50', 'opacity-0')
+        setTimeout(() => modal.classList.add('invisible'), 300)
+
+        video.src = video.src.replace('&autoplay=1', '').replace('?autoplay=1', '')
+
+        _unsetShim()
+    }
 
     trigger.map(el => el.addEventListener('click', e => on(e)))
     close.addEventListener('click', off)
