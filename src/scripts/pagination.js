51df11f1806cfec5a67c9b801086313d88be4675
K16E, Inc <kb@k16e.co>
2024-12-11 07:50:05 +0100

    ...

--- src/scripts/pagination.js
+++ src/scripts/pagination.js
@@ -1,6 +1,6 @@
 export function _paginateLink(baseURL, current) {
     const url = new URL(baseURL) // Create a URL object with the base URL
     url.searchParams.set('page', current) // Update the 'page' query parameter
-    console.log(url.toString())
+
     return url.toString() // Return the full URL as a string
 }
