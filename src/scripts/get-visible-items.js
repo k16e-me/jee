5ab553a33b1b9feeede0e6421ea8fdebd967d622
K16E, Inc <kb@k16e.co>
2024-12-14 14:39:22 +0100

    now added option to search category, if insights

--- src/scripts/get-visible-items.js
+++ src/scripts/get-visible-items.js
@@ -4,7 +4,8 @@ export function _getVisibleItems(totalItems, search, current, maxItems) {
         search.trim().length === 0 ||
         (item.content.name && item.content.name.toLowerCase().includes(search.toLowerCase())) ||
         (item.content.title && item.content.title.toLowerCase().includes(search.toLowerCase())) ||
-        (item.content.byline?.[0]?.name?.toLowerCase().includes(search.toLowerCase()) ?? false)
+        (item.content.byline?.[0]?.name?.toLowerCase().includes(search.toLowerCase()) ?? false) ||
+        (item.content.category?.name?.toLowerCase().includes(search.toLowerCase()) ?? false)
     )
     const visibleItems = filteredItems.slice(
         (current - 1) * maxItems,
