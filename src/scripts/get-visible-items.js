1db2c24c7aa552643a665cc1a51c6544b006f12d
K16E, Inc <kb@k16e.co>
2024-12-14 14:32:50 +0100

    now also directly searching designation, if people

--- src/scripts/get-visible-items.js
+++ src/scripts/get-visible-items.js
@@ -1,11 +1,15 @@
 export function _getVisibleItems(totalItems, search, current, maxItems) {
     const filteredItems = totalItems.filter((item) =>
-        !search || search.trim().length === 0 || (item.content.name && item.content.name.toLowerCase().includes(search.toLowerCase())) || (item.content.title && item.content.title.toLowerCase().includes(search.toLowerCase()))
+        !search ||
+        search.trim().length === 0 ||
+        (item.content.name && item.content.name.toLowerCase().includes(search.toLowerCase())) ||
+        (item.content.title && item.content.title.toLowerCase().includes(search.toLowerCase())) ||
+        (item.content.byline?.[0]?.name?.toLowerCase().includes(search.toLowerCase()) ?? false)
     )
     const visibleItems = filteredItems.slice(
         (current - 1) * maxItems,
         current * maxItems
     )
-    
+
     return { filteredItems, visibleItems }
 }
