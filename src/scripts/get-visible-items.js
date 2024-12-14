d837929d9db072cbfde2967fdfdd442198b16303
K16E, Inc <kb@k16e.co>
2024-12-14 18:12:27 +0100

    ...

--- src/scripts/get-visible-items.js
+++ src/scripts/get-visible-items.js
@@ -1,11 +1,11 @@
 export function _getVisibleItems(totalItems, search, current, maxItems) {
     const stemWord = (word) => {
-        return word.toLowerCase().replace(/(?:s|es)$/, '');
-    };
+        return word.toLowerCase().replace(/(?:s|es)$/, '')
+    }
 
     const matchWords = (searchTerm, content) => {
-        const searchWords = searchTerm.toLowerCase().split(/\s+/);
-        const contentWords = content.toLowerCase().split(/\s+/);
+        const searchWords = searchTerm.toLowerCase().split(/\s+/)
+        const contentWords = content.toLowerCase().split(/\s+/)
 
         return searchWords.every(searchWord =>
             searchWord.length === 1
@@ -14,8 +14,8 @@ export function _getVisibleItems(totalItems, search, current, maxItems) {
                     stemWord(contentWord).includes(stemWord(searchWord)) ||
                     stemWord(searchWord).includes(stemWord(contentWord))
                 )
-        );
-    };
+        )
+    }
 
     const filteredItems = totalItems.filter((item) =>
         !search ||
@@ -23,8 +23,9 @@ export function _getVisibleItems(totalItems, search, current, maxItems) {
         (item.content.name && matchWords(search, item.content.name)) ||
         (item.content.title && matchWords(search, item.content.title)) ||
         (item.content.byline?.[0]?.name && matchWords(search, item.content.byline[0].name)) ||
+        (item.content.author?.[0]?.name && matchWords(search, item.content.author[0].name)) ||
         (item.content.category?.name && matchWords(search, item.content.category.name))
-    );
+    )
 
     const visibleItems = filteredItems.slice(
         (current - 1) * maxItems,
