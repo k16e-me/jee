ec98dc9b21fab6aea940671d3f16d2508b7960ef
K16E, Inc <kb@k16e.co>
2024-12-22 16:34:58 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -7,7 +7,7 @@ const
     s4Upper = 'text-[1.715rem] leading-[1.9rem] md:text-[1.815rem]',
     sx1 = 'text-[1.015rem] leading-[1.345rem] md:text-[1.0625rem]',
     sx2 = 'text-[1.25rem] leading-[1.45rem] md:text-[1.3rem] md:leading-[1.65rem]',
-    copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.135rem] lg:leading-[1.65rem]',
+    copy = 'text-[1.0725rem] leading-[1.395rem] lg:text-[1.135rem] lg:leading-[1.55rem]',
     emphasis = 'text-[1.0525rem] leading-[1.475rem]',
     emphasisUpper = 'text-[0.875rem] leading-[1rem]',
     button = 'text-[0.935rem] leading-[1.175rem]',
