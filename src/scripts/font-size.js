b6267c488ef9ad480f5551d5afde74632cdb6ed0
K16E, Inc <kb@k16e.co>
2024-12-30 07:31:36 +0100

    ...

--- src/scripts/font-size.js
+++ src/scripts/font-size.js
@@ -1,7 +1,7 @@
 const
     s2 = 'text-3xl md:text-[3.675rem] md:leading-[3.925rem] lg:text-[4.15rem] lg:leading-[4.275rem]',
     s3 = 'text-[1.775rem] leading-[2rem] md:text-[2.275rem] md:leading-[2.5rem] lg:text-[3.015rem] lg:leading-[3.475rem]',
-    s4 = 'text-[1.55625rem] leading-[1.75rem] md:text-[1.795rem] md:leading-[1.875rem] lg:text-[1.9rem] lg:leading-[2.05rem]',
+    s4 = 'text-[1.55625rem] leading-[1.775rem] md:text-[1.795rem] md:leading-[1.975rem] lg:text-[1.9rem] lg:leading-[2.25rem]',
     s5 = 'text-[1.3125rem] leading-[1.45rem] md:text-[1.4625rem] md:leading-[1.75rem] lg:text-[1.5275rem] lg:leading-[1.7375rem]',
     s6 = 'text-[1.0875rem] leading-[1.3725rem]',
     s4Upper = 'text-[1.715rem] leading-[1.9rem] md:text-[1.815rem]',
