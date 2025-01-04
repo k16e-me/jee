94f4fe5b196de734b307bb28767612b44918494c
K16E, Inc <kb@k16e.co>
2025-01-04 22:16:53 +0100

    settings now served x own file

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,11 +1,8 @@
 import { atom } from 'nanostores'
 import { persistentAtom } from '@nanostores/persistent'
 
-export const
-    _settings = persistentAtom('settings', ''),
-    _shim = atom(false)
+export const _shim = atom(false)
 
-export function _addSettings(item) { _settings.set(item) }
 
 export function _setShim() { _shim.set(true) }
 export function _unsetShim() { _shim.set(false) }
