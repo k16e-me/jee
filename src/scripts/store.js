ff70ac3a080aaa831628f0893ef0ad4c85a7fd34
K16E, Inc <kb@k16e.co>
2025-01-04 22:34:40 +0100

    all page data refactored to scripts/collections

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,8 +1,6 @@
 import { atom } from 'nanostores'
-import { persistentAtom } from '@nanostores/persistent'
 
 export const _shim = atom(false)
-
 
 export function _setShim() { _shim.set(true) }
 export function _unsetShim() { _shim.set(false) }
