2c67a212e5451100539bf07f1f6dfd4c47cc6eeb
K16E, Inc <kb@k16e.co>
2025-02-12 21:28:09 +0100

    ...

--- src/scripts/lib/figure-image.js
+++ src/scripts/lib/figure-image.js
@@ -10,7 +10,7 @@ export function _figureImg() {
         figure.setAttribute('class', 'mx-[calc(-50vw+50%)] lg:ml-0 lg:mr-[calc(-50vw+75%)]')
         figure.innerHTML = `
             <img src="${i.src}"/>
-            <figcaption class="bg-[var(--color-bg-default-elevated)] block px-4 py-4 m-0 text-[var(--color-text-title)]">
+            <figcaption class="bg-(--color-bg-default-elevated) block px-4 py-4 m-0 text-[var(--color-text-title)]">
                 ${i.title}
             </figcaption>
         `
